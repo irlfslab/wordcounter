ecaaa0174233cfd9d4e38488e2f08c0eb6f42ae1
IL <irewlg@outlook.com>
2025-06-26 23:19:43 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css';
-import { Route, Routes } from "react-router";
+import {BrowserRouter, Routes, Route} from 'react-router'
 import { HomePage } from "./pages/HomePage";
 import { JokePage } from "./pages/JokePage";
 
 
 function App() {
   return (
-      <>
+   <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="joke" element={<JokePage />}></Route>
         </Routes>
-      </>
+  </BrowserRouter>
   );
 }
 
