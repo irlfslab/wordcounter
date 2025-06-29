eeb8f1e4ce13de5df50f7ecd032593d6a61e82f1
IL <irewlg@outlook.com>
2025-06-29 15:07:51 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router'
+import {Routes, Route} from 'react-router'
 import { HomePage } from "./pages/HomePage";
 import { JokePage } from "./pages/JokePage";
+import { PageNotFound } from "./pages/PageNotFound";
 
 
 function App() {
   return (
-   <BrowserRouter>
+   <>
         <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="joke" element={<JokePage />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
-  </BrowserRouter>
+  </>
   );
 }
 
